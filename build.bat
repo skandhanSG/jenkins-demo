978301dbe8da6ba1e5a8bab7eb7ef7e4151fa121
Skandhan Sivagnanam <skandhans@clouddestinations.com>
2026-01-16 17:18:47 +0530

    First Jenkins sample build file

---
+++ build.bat
@@ -0,0 +1,15 @@
+@echo off
+echo =========================
+echo Jenkins Sample Build
+echo =========================
+echo.
+echo This is running from Windows
+echo.
+echo Current Directory:
+cd
+echo.
+echo Files in folder:
+dir
+echo.
+echo Build completed successfully!
+pause
